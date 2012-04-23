eddfea6d2a79055925e1f1ca79f81f5bd2ee4b34
Jamie Rolfs <jr@wayin.com>
2012-04-22 18:42:07 -0600

    Finished setting up build process, optimized build now uses Almond in place of RequireJS. Development index is now index-dev.html which uses less.js to compile less styles at runtime.

---
+++ app/js/build.js
@@ -0,0 +1,37 @@
+({
+	appDir: '../',
+	baseUrl: 'js',
+	dir: '../../build',
+	mainConfigFile: 'main.js',
+	wrap: true,
+
+	paths: {
+		'almond': '../../node_modules/almond/almond',
+
+		// Libraries
+		'jquery': 'libs/jquery-1.7.2',
+		'underscore': 'libs/underscore-1.3.3',
+		'backbone': 'libs/backbone-0.9.2',
+		'handlebars': 'libs/handlebars-1.0.0.beta.6',
+		'xml2json': 'libs/jquery.xml2json',
+		
+		// Require
+		'use': 'libs/require/use-0.2.0',
+		'text': 'libs/require/text-1.0.7',
+
+		// Bootstrap
+		'transition': 'libs/bootstrap/bootstrap-transition',
+		'modal': 'libs/bootstrap/bootstrap-modal',
+		'dropdown': 'libs/bootstrap/bootstrap-dropdown',
+		'button': 'libs/bootstrap/bootstrap-button',
+		'collapsible': 'libs/bootstrap/bootstrap-collapse',
+		'tooltip': 'libs/bootstrap/bootstrap-tooltip',
+		'tabs': 'libs/bootstrap/bootstrap-tab'
+	},
+
+	modules: [{
+		name: 'main',
+		include: ['almond'],
+		out: 'main.js'
+	}]
+})
--- build.sh
+++ build.sh
@@ -2,25 +2,27 @@
 # Wayin integration Ask widget build
 
 echo -e "---------- *** Cleaning build directory *** ----------\n"
-rm -dfr ask-build
-mkdir -p ask-build/css
+rm -dfr build
+mkdir -p build/css
 
 # Process CSS
 echo -e "---------- *** Processing CSS with lessc *** ----------\n"
-node_modules/less/bin/lessc --yui-compress app/css/ask.less > ask-build/css/ask.css
+node_modules/less/bin/lessc --yui-compress app/less/plex.less > build/css/plex.css
 
 # Run the optimizer
 echo -e "---------- *** Running r.js optimizer *** ----------\n"
-node_modules/requirejs/bin/r.js -o app/js/ask.build.js
+node_modules/requirejs/bin/r.js -o app/js/build.js
 
 echo -e "---------- *** Removing unecessary files and folders *** ----------\n"
 # Remove unnecessary files
-rm ask-build/build.txt
-find ask-build -type f -name "*.less" -o -name "*.handlebars" -o -name "*-dev*" | xargs rm
-find ask-build -type f -name "*.js" -not -name "*main.js" -not -name "require*" | xargs rm
-
+rm build/build.txt
+find build -type f -name "*.less" -o -name "*.tpl" -o -name "*-dev*" | xargs rm
+# Currently building with Almond, if we need RequireJS features that Almond
+# does not support, make sure to add `-not -name "require*"` to this find
+find build -type f -name "*.js" -not -name "main.js" | xargs rm
 # Remove empty folders
-find ask-build -depth -empty -type d -exec rmdir {} \;
+find build -depth -empty -type d -exec rmdir {} \;
 
-echo -e "---------- *** Renaming index.html *** ----------\n"
-mv ask-build/index-ask.html ask-build/index.html
+# Use this if development index is named something besides index.html
+#echo -e "---------- *** Renaming index.html *** ----------\n"
+#mv build/myindex.html build/index.html
