cf7167dd3643dd4fed5b265bb5f1dd446e4e450a
Tom Gallacher <tomgallacher23@gmail.com>
2012-05-15 11:41:20 +0100

    Quick tidy up of codebase using app.configure(<ENV>)

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-var port = process.env.PORT || 3000;
-var index;
-
-var NODE_ENV = process.env.NODE_ENV || 'development';
-
 var express = require('express'),
 	httpProxy = require('http-proxy'),
 	routingProxy = new httpProxy.RoutingProxy(),
-	app = express.createServer();
+	app = express.createServer(),
+	port = process.env.PORT || 3000,
+	watch,
+	index,
+	less = require('less'),
+	cssDirectory = './app/less',
+	cssRootFile = 'plex.less';
 
-if (NODE_ENV === 'development') {
-	var less = require('less');
-	var watch = require('watch');
-	var cssDirectory = './app/less';
-	var cssRootFile = 'plex.less';
-}
-
-console.log('Starting Plex Client web server on port ' + port + ' in \'' + NODE_ENV + '\' environment');
+console.log('Starting Plex Client web server on port ' + port + ' in \'' + process.env.NODE_ENV + '\' environment');
 
 function apiProxy(pattern) {
 	return function (req, res, next) {
@@ -39,55 +33,56 @@ app.configure(function () {
 	app.use(express.bodyParser());
 	app.use(express.static(__dirname));
 	app.use(express.errorHandler({
-		dumpExceptions: true, 
+		dumpExceptions: true,
 		showStack: true
 	}));
 	app.use(app.router);
 });
 
-switch (NODE_ENV) {
-	case 'production':
-		index = 'https://plex.herokuapp.com/index.html';
-		break;
-	case 'development':
-		if (watch) {
-			console.log('Development CSS processing active, monitoring "' + cssRootFile + '" in "' + cssDirectory + '"');
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	watch = require('watch');
+	index = '/app/index-dev.html';
+	watchFiles();
+});
 
-			var parser = new less.Parser({
-				paths: [cssDirectory],
-				filename: './app/less/plex.less	'
-			});
+app.configure('production', function(){
+	index = 'https://plex.herokuapp.com/index.html';
+});
 
-			var parse = function () {
-				parser.parse('', function (error, tree) {
-					if (error) return console.log(error);
-					console.log(tree.toCSS());
-				});
-			}
+function watchFiles() {
+	console.log('Development CSS processing active, monitoring "' + cssRootFile + '" in "' + cssDirectory + '"');
 
-			watch.createMonitor(cssDirectory, function (monitor) {
-				monitor.on("created", function (file, stat) {
-					console.log('CSS processing detected new file "' + file + '", generating new css file');
-					parse();
-				});
-				monitor.on("changed", function (file, current, previous) {
-					console.log('CSS processing detected file change "' + file + '", generating new css file');
-					parse();
-				});
-				monitor.on("removed", function (file, stat) {
-					console.log('CSS processing detected deleted file "' + file + '", generating new css file');
-					parse();
-				});
-			});
-		}
-	default:
-		index = '/app/index-dev.html';
-		break;
+	var parser = new less.Parser({
+		paths: [cssDirectory],
+		filename: './app/less/plex.less'
+	});
+
+	var parse = function () {
+		parser.parse('', function (error, tree) {
+			if (error) return console.log(error);
+			console.log(tree.toCSS());
+		});
+	};
+
+	watch.createMonitor(cssDirectory, function (monitor) {
+		monitor.on("created", function (file, stat) {
+			console.log('CSS processing detected new file "' + file + '", generating new css file');
+			parse();
+		});
+		monitor.on("changed", function (file, current, previous) {
+			console.log('CSS processing detected file change "' + file + '", generating new css file');
+			parse();
+		});
+		monitor.on("removed", function (file, stat) {
+			console.log('CSS processing detected deleted file "' + file + '", generating new css file');
+			parse();
+		});
+	});
 }
 
 app.get('/', function (req, res) {
 	res.redirect(index);
 });
-
 
 app.listen(port);
